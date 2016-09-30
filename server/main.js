14955e425a58b91abb5f6963be9cf9a49d6fba4b
Pierre Baize <pierrebaize@Pierres-MacBook-Pro.local>
2016-09-30 10:57:22 -0400

    https working

--- server/main.js
+++ server/main.js
@@ -1,24 +1,43 @@
 'use strict'
 var chalk = require('chalk')
 var db = require('./db')
+var https = require('https')
+var fs = require('fs')
+var path = require('path')
 
 // Create a node server instance! cOoL!
-var server = require('http').createServer()
+// var server = require('http').createServer()
+
+var options = {
+  key: fs.readFileSync(path.join(__dirname, '/../ssl/key.pem')),
+  ca: fs.readFileSync(path.join(__dirname, '/../ssl/csr.pem')),
+  cert: fs.readFileSync(path.join(__dirname, '/../ssl/cert.pem'))
+}
+
+var app = require('./app')(db)
+var PORT = process.env.PORT || 1337
+var server = https.createServer(options)
 
 var createApplication = function () {
-  var app = require('./app')(db)
   server.on('request', app) // Attach the Express application.
-  require('./io')(server) // Attach socket.io.
+  // require('./io')(server) // Attach socket.io.
 }
 
-var startServer = function () {
-  var PORT = process.env.PORT || 1337
+var startServer = server
+ .listen(PORT, '', null, function () {
+   console.log(chalk.blue('Server started on port', chalk.magenta(PORT)))
+ })
 
-  server.listen(PORT, function () {
-    console.log(chalk.blue('Server started on port', chalk.magenta(PORT)))
+// var startServer = function () {
+//   var PORT = process.env.PORT || 1337
+
+//   server.listen(PORT, function () {
+//     console.log(chalk.blue('Server started on port', chalk.magenta(PORT)))
+//   })
+// }
+
+db.sync()
+  .then(createApplication)
+  .then(startServer).catch(function (err) {
+    console.error(chalk.red(err.stack))
   })
-}
-
-db.sync().then(createApplication).then(startServer).catch(function (err) {
-  console.error(chalk.red(err.stack))
-})
